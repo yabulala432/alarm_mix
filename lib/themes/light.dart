f4084116910b1ce6b3256ae8f0b94ba187b6f2f4
Yeabsira Yonas <yabulala432@gmail.com>
2024-11-08 12:06:53 +0300

    withOpacity is deprecated in the updated flutter

--- lib/themes/light.dart
+++ lib/themes/light.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 ThemeData light = ThemeData(
-  colorScheme: ColorScheme.light(
-    surface: const Color.fromARGB(255, 235, 201, 173),
-    shadow: const Color.fromARGB(255, 0, 0, 0).withOpacity(.3),
-    primary: const Color.fromARGB(255, 218, 210, 206),
-    secondary: const Color.fromARGB(139, 40, 71, 135),
-    inversePrimary: const Color.fromARGB(255, 142, 126, 112),
+  colorScheme: const ColorScheme.light(
+    surface: Color.fromARGB(255, 235, 201, 173),
+    shadow: Color.fromARGB(255, 0, 0, 0),
+    primary: Color.fromARGB(255, 218, 210, 206),
+    secondary: Color.fromARGB(139, 40, 71, 135),
+    inversePrimary: Color.fromARGB(255, 142, 126, 112),
     onPrimary: Colors.white,
-    error: const Color.fromARGB(255, 125, 48, 48),
+    error: Color.fromARGB(255, 125, 48, 48),
   ),
 );
